1ee9dcb1bbf0c94c35e7ca7c2025a078de683a0c
Thomas Winged <23487476+thomaswinged@users.noreply.github.com>
2022-09-15 08:20:11 +0200

    convert greyscale to rgb

--- util/io.py
+++ util/io.py
@@ -190,6 +190,9 @@ def write_depth(path, depth, bits=1, absolute_depth=False):
         else:
             out = np.zeros(depth.shape, dtype=depth.dtype)
 
+    # Convert greyscale to RGB
+    out = cv2.cvtColor(out,cv2.COLOR_GRAY2RGB)
+
     if bits == 1:
         cv2.imwrite(path + ".png", out.astype("uint8"), [cv2.IMWRITE_PNG_COMPRESSION, 0])
     elif bits == 2:
